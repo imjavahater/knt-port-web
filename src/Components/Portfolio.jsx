a78466de4d8e78ae1c67d7918a82875c2daf2137
imjavahater <71551477+imjavahater@users.noreply.github.com>
2023-10-29 09:30:13 +0000

    No " that why it caused error.

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/programmer.jpg;
+import image from "../images/programmer.jpg";
 
 const imageAltText = "vector picture with laptop and User interface";
 
