86bdcef716299922bb4fd50aa4d3dfa9ecfc2741
imjavahater <71551477+imjavahater@users.noreply.github.com>
2023-10-29 09:47:56 +0000

    Include the State and Country

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm currently full-time Analyst in a Financial-Technology company based in Norwalk. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm currently full-time Analyst in a Financial-Technology company based in Connecticut, USA. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
